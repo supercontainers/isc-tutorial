76220fd0c63989d3cdf6274efd0b466986b448c4
Marco De La Pierre <marco.delapierre@gmail.com>
2021-06-24 17:53:43 +0800

    update mpich path in vm

--- exercises/openfoam_wing/mpirun/mpirun.sh
+++ exercises/openfoam_wing/mpirun/mpirun.sh
@@ -4,7 +4,7 @@ NTASKS="4"
 export theImage="library://marcodelapierre/beta/openfoam:v2012"
 
 # this configuration depends on the host
-export MPICH_ROOT="/opt/intel/oneapi"
+export MPICH_ROOT="/opt/spack" #intel/oneapi"
 export MPICH_LIBS="$( which mpirun )"
 export MPICH_LIBS="${MPICH_LIBS%/bin/mpirun*}/lib"
 
