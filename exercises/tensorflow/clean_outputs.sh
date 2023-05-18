474bb281d3e516894bd4da7064b560cf0dbca569
Marco De La Pierre <marco.delapierre@gmail.com>
2023-05-18 15:01:56 +0200

    ep 14: edit to clean output script
    Signed-off-by: Marco De La Pierre <marco.delapierre@gmail.com>

--- exercises/tensorflow/clean_outputs.sh
+++ exercises/tensorflow/clean_outputs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -rf fake_home/* ; touch fake_home/.keep
+rm -rf fake_home/* fake_home/.keras ; touch fake_home/.keep
 rm -rf logs
 rm -rf saved_model
 rm -rf training_checkpoints
-rm slurm*.out
+rm -f slurm*.out
